173233748caccc62d8d2fd65ae02281ded2f3e85
mesn1985 <lightmaze16@hotmail.com>
2024-04-21 13:25:14 +0200

    host name added to identity server docker compose

--- msdo.oauth.identityServer/Startup.cs
+++ msdo.oauth.identityServer/Startup.cs
@@ -29,6 +29,7 @@ namespace msdo.oauth.identityServer
             {
                 // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
                 options.EmitStaticAudienceClaim = true;
+
             })
                 .AddInMemoryIdentityResources(Config.IdentityResources)
                 .AddInMemoryApiScopes(Config.ApiScopes)
