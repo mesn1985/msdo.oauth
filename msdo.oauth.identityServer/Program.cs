5634c2641d4319431122713b050736d868b99828
mesn1985 <lightmaze16@hotmail.com>
2024-05-08 14:08:29 +0200

    Graylog sinks added to identity server

--- msdo.oauth.identityServer/Program.cs
+++ msdo.oauth.identityServer/Program.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using System;
-using System.IO;
 using Microsoft.Extensions.Configuration;
 using Serilog.Events;
 using Serilog.Sinks.SystemConsole.Themes;
